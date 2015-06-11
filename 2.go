cc08733065c32490b89c397f78b14a10b9481bc8
Ivelin Penchev <ip@inocr8.co>
2015-06-11 11:56:51 +0100

    Added solutions to the first two problems

--- 2.go
+++ 2.go
@@ -3,10 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	upTo := 1000
+	upTo := 4000000
 	sum := 0
-	for i := 1; i < upTo; i++ {
-		if i%3 == 0 || i%5 == 0 {
+	prev := 1
+	for i := 2; i < upTo; i, prev = i+prev, i {
+		if i%2 == 0 {
 			sum += i
 		}
 	}
